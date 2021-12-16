52f00b118cb2c63c944973d146f1576d055f424a
Nishant Sorathiya <nishant.sorathiya2@gmail.com>
2021-12-16 23:19:00 +0530

    test

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -24,7 +24,7 @@ class UserProfileManger(BaseUserManager):
 
     def create_superuser(self,email,name,password):
         """ cerate new super user with given details"""
-        user =self.create_use(email,name,password)
+        user =self.create_user(email,name,password)
 
         user.is_superuser=True
         user.is_staff=True
